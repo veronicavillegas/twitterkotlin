406b0eff8ebebb535161bf60a56e149a44b98e21
veronica.villegas <veronica.villegas@etermax.com>
2021-07-28 18:03:04 -0300

    Mock userRepo in updateUserTest

--- src/test/kotlin/actions/users/UpdateUserTest.kt
+++ src/test/kotlin/actions/users/UpdateUserTest.kt
@@ -8,23 +8,30 @@ import com.twitterkata.domain.users.actions.GetUser
 import org.junit.jupiter.api.Test
 import com.twitterkata.domain.users.actions.UpdateUser
 import com.twitterkata.domain.users.repositories.UserInMemoryRepository
+import com.twitterkata.domain.users.repositories.UserRepository
+import com.twitterkata.infraestructure.DataBaseConnection
+import org.mockito.Mockito
+import kotlin.test.BeforeTest
 import kotlin.test.assertEquals
 
 internal class UpdateUserTest {
-    private var updateUser = UpdateUser(UserInMemoryRepository())
+    private val userRepository = Mockito.mock(UserRepository::class.java)
+    private val updateUser = UpdateUser(userRepository)
+    private val user = User("Veronica", "Villegas", "@vero")
 
     @Test
     fun updateInexistentUser_thenInexistentUserError() {
-        val result = updateUser(User("Yanina", "Rodriguez", "@inexistent"))
-
+        Mockito.`when`(userRepository.get(user.nickname)).thenReturn(null)
+        val result = updateUser(user)
         assertEquals(Status.FAIL, result.status)
         assertEquals(Messages.INEXISTENT_USER, result.message)
     }
 
     @Test
     fun updateUser_thenUserIsUpdated() {
-        updateUser(User("Veronica", "Villegas", "@vero"))
-        val response = updateUser(User("Maria", "Rodriguez", "@vero"))
+        val savedUser = User("maria", "perez", "@vero")
+        Mockito.`when`(userRepository.get(user.nickname)).thenReturn(savedUser)
+        val response = updateUser(user)
         assertEquals(Status.OK, response.status)
         assertEquals(Messages.OK, response.message)
     }
