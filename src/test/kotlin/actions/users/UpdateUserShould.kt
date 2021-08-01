3b9dadd70d5ac1fe1eb64d006899db814887f017
veronica.villegas <veronica.villegas@etermax.com>
2021-08-01 10:48:24 -0300

    Actualizo update user con nuevo idGeneratior

--- src/test/kotlin/actions/users/UpdateUserShould.kt
+++ src/test/kotlin/actions/users/UpdateUserShould.kt
@@ -1,25 +1,27 @@
 package actions.users
 
-import com.twitterkata.domain.enums.Messages
-import com.twitterkata.domain.enums.Status
 import com.twitterkata.domain.users.InexistentUserException
 import com.twitterkata.domain.users.User
-import com.twitterkata.domain.users.actions.GetUser
 
 import org.junit.jupiter.api.Test
 import com.twitterkata.domain.users.actions.UpdateUser
-import com.twitterkata.domain.users.repositories.UserInMemoryRepository
 import com.twitterkata.domain.users.repositories.UserRepository
-import com.twitterkata.infraestructure.DataBaseConnection
+import com.twitterkata.infraestructure.IDGenerator
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.assertThrows
 import org.mockito.Mockito
-import kotlin.test.BeforeTest
-import kotlin.test.assertEquals
 
 internal class UpdateUserShould {
     private val userRepository = Mockito.mock(UserRepository::class.java)
     private val updateUser = UpdateUser(userRepository)
-    private val user = User("Veronica", "Villegas", "@vero")
+    private val uuid = Mockito.mock(IDGenerator::class.java)
+    private val idGenerated = "123"
+    private val user = User("Veronica", "Villegas", "@vero", idGenerated)
+
+    @BeforeEach
+    fun setUp() {
+        Mockito.`when`(uuid.generateId()).thenReturn(idGenerated)
+    }
 
     @Test
     fun throwExceptionWhenUserNotExists() {
@@ -29,7 +31,7 @@ internal class UpdateUserShould {
 
     @Test
     fun invokeUserRepoUpdateWhenUpdateUser() {
-        val savedUser = User("maria", "perez", "@vero")
+        val savedUser = User("maria", "perez", "@vero", idGenerated)
         Mockito.`when`(userRepository.get(user.nickname)).thenReturn(savedUser)
         updateUser(user)
         Mockito.verify(userRepository).update(user)
