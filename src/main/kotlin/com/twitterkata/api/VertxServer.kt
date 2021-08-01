1e2fa6ed3537d60da262bdf7085edfc203cb34ac
veronica.villegas <veronica.villegas@etermax.com>
2021-08-01 10:49:55 -0300

    CC vertx server

--- src/main/kotlin/com/twitterkata/api/VertxServer.kt
+++ src/main/kotlin/com/twitterkata/api/VertxServer.kt
@@ -3,14 +3,17 @@ package com.twitterkata.api
 import io.vertx.core.AbstractVerticle
 import io.vertx.core.Vertx
 import io.vertx.ext.web.Router
+import io.vertx.ext.web.handler.BodyHandler
 
 class VertxServer: AbstractVerticle()  {
     val port = 8888
     val vertxServer = Vertx.vertx()
 
     override fun start() {
-        val router = TwitterRouter(vertxServer).invoke()
+        val router = Router.router(vertx)
         createServer(router)
+        router.route().handler(BodyHandler.create())  // Required for RoutingContext.bodyAsString
+        TwitterRouter(vertxServer).applyRoutes(router)
     }
 
     private fun createServer(router: Router) {
