2a621f5cedf9aeca7e861378ac8d64b8b4aea080
veronica.villegas <veronica.villegas@etermax.com>
2021-08-01 10:48:46 -0300

    Test para RegisterUserHandler

---
+++ src/test/kotlin/api/handlers/RegisterUserShould.kt
@@ -0,0 +1,20 @@
+package api.handlers
+
+import com.twitterkata.api.handlers.RegisterUserHandler
+import io.vertx.ext.web.RoutingContext
+import org.junit.jupiter.api.Test
+import org.mockito.Mockito
+import org.mockito.Mockito.mock
+import kotlin.test.Ignore
+
+class RegisterUserShould {
+    val registerUser = RegisterUserHandler()
+    val event = mock(RoutingContext::class.java)
+
+    fun mapCorrectRegisterUserData() {
+        val registerUserData = "{\n\t\"nickname\": \"@vero\", \n\t\"surname\": \"villegas\", \n\t\"firstname\": \"vero\"\n}"
+        Mockito.`when`(event.bodyAsString).thenReturn(registerUserData)
+        registerUser.handle(event)
+
+    }
+}
