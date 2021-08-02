298502d37d6d6b643ff41aa260c3bdf597838439
veronica.villegas <veronica.villegas@etermax.com>
2021-08-02 15:55:52 -0300

    Router

--- src/main/kotlin/com/twitterkata/api/TwitterRouter.kt
+++ src/main/kotlin/com/twitterkata/api/TwitterRouter.kt
@@ -1,6 +1,11 @@
 package com.twitterkata.api
 
 import com.twitterkata.api.handlers.*
+import com.twitterkata.domain.users.actions.RegisterUser
+import com.twitterkata.domain.users.repositories.UserMySqlRepository
+import com.twitterkata.infraestructure.JsonVertxUtility
+import com.twitterkata.infraestructure.MySqlConnection
+import com.twitterkata.infraestructure.UUIDGenerator
 import io.vertx.core.Vertx
 import io.vertx.ext.web.Router
 
@@ -8,9 +13,15 @@ class TwitterRouter(private val vertx: Vertx) {
     val kataTwitter = "/katatwitter"
 
     fun applyRoutes(router: Router): Router = router.apply {
+        val jsonUtility = JsonVertxUtility()
+        val connection = MySqlConnection()
+        //connection.initConnection()
+        val userRepository = UserMySqlRepository(connection)
+        val registerUser = RegisterUser(userRepository = userRepository, idGenerator = UUIDGenerator())
+
         post("$kataTwitter/users")
             .consumes("application/json")
-            .handler{ context -> RegisterUserHandler().handle(context)}
+            .handler{ context -> RegisterUserHandler(registerUser, jsonUtility).handle(context)}
         //get("$kataTwitter/users/:nickname").handler(GetUser().handle())
         //put("$kataTwitter/users").handler(UpdateUser())
         //put("$kataTwitter/users/follow").handler(FollowUser())
