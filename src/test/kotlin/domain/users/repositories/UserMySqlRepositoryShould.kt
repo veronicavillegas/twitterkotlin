d840316f7258243f7e9bc3473879dd0a31102c4d
veronica.villegas <veronica.villegas@etermax.com>
2021-08-02 11:24:02 -0300

    Update UserMySqlRepositoryShould.kt

--- src/test/kotlin/domain/users/repositories/UserMySqlRepositoryShould.kt
+++ src/test/kotlin/domain/users/repositories/UserMySqlRepositoryShould.kt
@@ -2,13 +2,14 @@ package domain.users.repositories
 
 import com.nhaarman.mockitokotlin2.verify
 import com.twitterkata.domain.users.User
+import com.twitterkata.domain.users.repositories.UserMySqlRepository
 import com.twitterkata.infraestructure.DataBaseConnection
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito
 
 class UserMySqlRepositoryShould {
     val dataBaseConnection = Mockito.mock(DataBaseConnection::class.java)
-    val userMySqlRepository = domain.users.repositories.UserMySqlRepository(connection = dataBaseConnection)
+    val userMySqlRepository = UserMySqlRepository(connection = dataBaseConnection)
 
     @Test
     fun saveUser() {
